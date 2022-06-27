df9993f7b46fbd5e34e7e6d37f4d6b707a7869e2
tea222 <danilkozyrev673@gmail.com>
2022-06-27 22:29:29 +0300

    add sfml support, add testing code

--- five_days_engine/main.cpp
+++ five_days_engine/main.cpp
@@ -1,12 +1,58 @@
 #include <iostream>
+#include "SFML/Graphics.hpp"
+
 
 
 int main() {
+    setlocale(LC_ALL, "Russian");
 
+    sf::RenderWindow window;
+    window.create(sf::VideoMode(800, 600), "5DaysEngine");
+    window.setVerticalSyncEnabled(true);
 
+    sf::Font font;
+    font.loadFromFile("Resources/OnestRegular1602-hint.ttf");
 
-    std::cout << "hello world\n";
-    system("pause");
+    // create a text to render
+    sf::Text text;
+    text.setFont(font);
+    text.setPosition(sf::Vector2f(window.getSize().x / 2.0f, window.getSize().y / 2.0f));
+    text.setString(L"fsdfhsdjkfhytureio, פûמאנגהûכמא48\n35נמכןûגאנמןא, 5648/*/+¹%;?({})(*@#% ");
+    
+    // main loop
+    while (window.isOpen())
+    {
+        // events
+        sf::Event e;
+        while (window.pollEvent(e))
+        {
+            switch (e.type) {
+            case sf::Event::Closed: // window closed
+                window.close();
+                break;
+            case sf::Event::KeyPressed: // key pressed
+                switch (e.key.code)
+                {
+                case sf::Keyboard::Space: // space
+                {
+                    // rotate text by 2 degree
+                    static float rotation = 0.0f;
+                    rotation = rotation >= 360.0f ? 0.0f : rotation + 2.0f;
+                    text.setRotation(rotation);
+                    break; 
+                }
+
+                default:
+                    break;
+                }
+            }
+        }
+
+        // render 
+        window.clear(sf::Color::Black);
+        window.draw(text);
+        window.display();
+    }
 
     return 0;
 }
